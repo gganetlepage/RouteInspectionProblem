0666fb305999027de02db169b3b59818a89b3a96
guiganlep <g.ganet.lepage@gmail.com>
2019-09-07 16:54:56 +0200

    add_list_into_list working

--- main.py
+++ main.py
@@ -348,12 +348,14 @@ def vertices_of_a_edge_list(cycle, new_edges):
     for i in range(size): #create a list indicating the vertices and the edge concerned
         edge_index = cycle[i]
         edge = new_edges[edge_index]
-        vertices_list.append([edge[0], edge_index])
-        vertices_list.append([edge[1], edge_index])
-        
+        vertices_list.append([edge[0], edge_index, i]) # i is the edge index in the cycle
+        vertices_list.append([edge[1], edge_index, i])
     return vertices_list
 
 
+""""
+Next step: Create the vertices list associated to a cycle list
+"""
 
 def partition(vertices_list, low, high):
     pivot = vertices_list[high][0] # element used for comparaison
@@ -393,14 +395,124 @@ def vertices_cycle(cycle_list, new_edges):
         vertices_list.append(reduced_list)
     return vertices_list
     
+"""
+Next step: with 2 cycles, being able to determine where to insert the first one into the second one
+"""
+
+def vertex_in_list(vertex, vertices_list): # is the vertex present in the list? return a bool
+    present = False
+    i = 0
+    size = len(vertices_list)
+    while not present and i < size:
+        if vertices_list[i][0] == vertex: #[0] due to working with list of 2 elements
+            present = True
+        else:
+            i += 1
+    return present, i #bool and vertex index
+
+def comparison_vertices_list(vertices_list1, vertices_list2):
+    present = False
+    i_list2 = 0 # index in list2 of a matching vertex with list1
+    i = 0
+    size = len(vertices_list1)
+    while not present and i< size:
+        present, i_list2 = vertex_in_list(vertices_list1[i][0], vertices_list2) #[0] due to working with list of 2 elements
+        if not present:
+            i+=1
+    return present, i, i_list2 # bool and possible vertices indices 
 
 
+def entering_or_outgoing(i, reduced_list, cycle_list ,new_edges): # True = entering 
+    vertex = reduced_list[i][0]
+#    edge = reduced_list[i][1]
+    index_in_cycle_list = reduced_list[i][2]
+#    edge_index = cycle_list[index_in_cycle_list]
+    precedent_edge_index = cycle_list[index_in_cycle_list - 1]
+    if new_edges[precedent_edge_index][0] == vertex or new_edges[precedent_edge_index][1] == vertex:
+        return False # outgoing
+    else:
+        return True # entering
 
 
+def real_order_of_cycle(precedent, cycle_list,edge_index_in_cycle): # edge_index_in_cycle = reduced_list[index][2]
+    correct_order_cycle = []
+    size = len(cycle_list)
+    if precedent: # then the edge must be put at the ultimate end, the cycle first leaves the concertex vertex then go back into it
+        for i in range(edge_index_in_cycle +1, size):
+            correct_order_cycle.append(cycle_list[i])
+        for i in range(edge_index_in_cycle+1):
+            correct_order_cycle.append(cycle_list[i])
+    else:
+#        for i in range(edge_index_in_cycle, edge_index_in_cycle - size, -1): 
+        for i in range(edge_index_in_cycle - size, edge_index_in_cycle): 
+            correct_order_cycle.append(cycle_list[i])
+    return correct_order_cycle
+
+    
+def list2_into_list1(cycle_list1, real_order_cycle_list2, precedent_list1, edge_index_in_cycle1):
+    if precedent_list1: 
+        # then list2 is added after the edge
+        #It is not possible for the edge_index_in_cycle1 to be the last element of the cycle_list1, that would mean the list2 can also be added after the first element
+        for x in real_order_cycle_list2:
+            # list2 is added in reverses
+            cycle_list1.insert(edge_index_in_cycle1+1, x)
+    else:
+        for x in real_order_cycle_list2:
+            cycle_list1.insert(edge_index_in_cycle1, x)
+    return cycle_list1
+    
+
+#delete list1? après la mathode list2_into_list1
+        
+    
+# this method is not finished 
+def add_list_into_list(cycle_list1, cycle_list2, vertices_list1, vertices_list2, new_edges, i_list1, i_list2):
+#    vertices_list1 = vertices_cycle(cycle_list1, new_edges) vertices_list1 is already reduced_list1 
+#    vertices_list2 = vertices_cycle(cycle_list2, new_edges) vertices_list2 is already reduced_list2
+    edge_index_in_cycle1 = vertices_list1[i_list1][2] # TODO why not use directly i_list or i_list2 ?
+    edge_index_in_cycle2 = vertices_list2[i_list2][2]
+    precedent_list1 = entering_or_outgoing(i_list1, vertices_list1, cycle_list1, new_edges)
+    precedent_list2 = entering_or_outgoing(i_list2, vertices_list2, cycle_list2, new_edges)
+    # arbitrary choice to insert list2 into list1, as list1 is generally longer on tested cases
+    correct_order_cycle_list2 = real_order_of_cycle(precedent_list2, cycle_list2, edge_index_in_cycle2)
+    concatenated_list = list2_into_list1(cycle_list1, correct_order_cycle_list2, precedent_list1, edge_index_in_cycle1)
+    return concatenated_list
 
 
+# this method is not finished
+def put_all_cycles_into_one(cycle_list, vertices_list, new_edges):
+    #vertices_list is reduced_list
+    
+    #loop WHILE here to iterate on every additional cycle
+    if len(vertices_list) >= 2:
+        vertices_list1 = vertices_list[0]
+        vertices_list2 = vertices_list[1]
+        present, i_list1, i_list2 = comparison_vertices_list(vertices_list1, vertices_list2)
+        if present:
+#            cycle_list # why is it here ?
+            concatenated_list = add_list_into_list(cycle_list[0], cycle_list[1], vertices_list1, vertices_list2, new_edges, i_list1, i_list2)
+            return concatenated_list
+        else:
+            return "there isn't a match there for the moment"
+    else:
+        return cycle_list #there is only one cycle or none
+    
 
+# TODO put the comparison_vertices_list method out of the method
 
+    
+        
+    
+         
+#def identification_of_matching_vertices():     
+    
+#il faut insérer la seconde liste après l'edge entering, et avant l'edge outgoing    
+    
+    
+    
+    
+    
+#vetices_of_an_edge
 
 def solving(vertices, edges):
     vertices_deg = vertices_degree(vertices, edges)
@@ -412,17 +524,32 @@ def solving(vertices, edges):
     new_edges = graph_with_added_edges(correct_perfect_matching, predecessor_matrix, previous_edge_matrix, edges)
     new_vertice_deg = vertices_degree(vertices, new_edges)
     new_edges_of_each_vertex = edges_of_each_vertex(vertices, new_edges, new_vertice_deg)
-    
+    #working on list of edges:    
     cycle_list1 = cycle_list(new_edges, new_edges_of_each_vertex)
     vertices_list = vertices_cycle(cycle_list1, new_edges)
+    # identifying matching vertices, DANGER how to behave if only one element in the list ?
+    
+    concatenated_list = put_all_cycles_into_one(cycle_list1, vertices_list, new_edges)
+    
+    
+    
+    
+#    if len(vertices_list) >= 2:
+#        present, i, j = comparison_vertices_list(vertices_list[0], vertices_list[1])
+#        if entering_or_outgoing(i, vertices_list[0],cycle_list1[0], new_edges):
+#            return True
+#        return vertices_list, present, i,j
+#    
     
     
 
-    return vertices_list
+    return concatenated_list
     
     
 
 start = time.time()
+
+
 
 
 vertices, edges = parse_file("paris_map.txt") # sys.argv[1]
@@ -456,6 +583,12 @@ e_vertices = [0,1,2,3,4,5,6,7]
 e_edges = [[0,1,1], [1,2,1], [2,3,1], [3,4,1], [2,5,1], [4,6,1], [6,7,1]]
 e_solution = solving(e_vertices, e_edges)
 print(e_solution)
+
+
+
+
+
+
 #vertices_degree_E = vertices_degree(e_vertices, e_edges)
 #odd_degree_vertices_E = odd_degree_vertices(vertices_degree_E)
 #edges_of_each_vortex_E = edges_of_each_vertex(e_vertices,e_edges,vertices_degree_E)
